d69a89c119b3998786cc9640a3fc0ac31ba80597
Muddapu akash <120655408+Muddapuakash@users.noreply.github.com>
2026-04-23 22:45:29 +0530

    Arrays Programs Basic to Advanced

---
+++ arrays/LargestSmallestElement.java
@@ -0,0 +1,22 @@
+package com.arrays;
+
+public class LargestSmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 1, 9, 4};
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+        System.out.println("Smallest: " + smallest);
+    }
+}
---
+++ arrays/MoveZerosToEnd.java
@@ -0,0 +1,25 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class MoveZerosToEnd {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}

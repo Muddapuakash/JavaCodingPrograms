e9712005f11279067c974c7d38e9297e2b562bd3
Muddapu akash <120655408+Muddapuakash@users.noreply.github.com>
2026-04-23 21:40:08 +0530

    CheckPalindromeString

---
+++ strings/CheckPalindromeString.java
@@ -0,0 +1,23 @@
+package com.strings;
+
+import java.util.Scanner;
+
+public class CheckPalindromeString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String original=str;
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--) {
+			rev=rev+str.charAt(i);
+		}
+		if(original.equals(rev)) {
+			System.out.println("Palindrome");
+		}else {
+			System.out.println("Not Palindrome");
+		}
+
+	}
+
+}
